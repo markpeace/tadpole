d076b3401ca59dc28095306fc5a3c56caad1813a
markpeace <mark@learnsomestuff.com>
2013-08-02 12:24:17 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tadpole::Application.config.secret_key_base = 'a62a1a9281c5b5259486df70a3c3914ff81d47113472ac2f18bd2b7813515d8238772dea3fc4efbd2c137e1448cf311bbfbe6ecf8477ae68743b93a9ef62709b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tadpole::Application.config.session_store :cookie_store, key: '_tadpole_session'
