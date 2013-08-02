2d3c0d07d1370328c9ffc29d21385aaf94c6edfa
markpeace <mark@learnsomestuff.com>
2013-08-02 15:24:34 +0100

    temporary splash screen

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Tadpole::Application.routes.draw do
   get "splash/index"
+  root 'splash#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
