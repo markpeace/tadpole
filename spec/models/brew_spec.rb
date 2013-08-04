10bcd53d97a307fe01e0e457f739e2773056bfca
markpeace <mark@learnsomestuff.com>
2013-08-04 10:04:41 +0100

    basic brew spec

--- spec/models/brew_spec.rb
+++ spec/models/brew_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Brew do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+	it { should belong_to :user }
+	it { should validate_presence_of :user }
+	
+	it {should validate_presence_of :name }
+	
+	pending "write a validator to check presence of brew name in brewtoad"
+	pending "write a function to retreive a hash of data from brewtoad on the brew"
+
 end
