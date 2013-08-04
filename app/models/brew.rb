67e33ac7d04c5341bfae7a1a0d383b5d37149a38
markpeace <mark@learnsomestuff.com>
2013-08-04 10:16:26 +0100

    specs green

--- app/models/brew.rb
+++ app/models/brew.rb
@@ -1,2 +1,6 @@
 class Brew < ActiveRecord::Base
+	belongs_to :user
+	validates_presence_of :user
+	
+	validates_presence_of :name
 end
--- spec/models/brew_spec.rb
+++ spec/models/brew_spec.rb
@@ -5,7 +5,7 @@ describe Brew do
 	it { should belong_to :user }
 	it { should validate_presence_of :user }
 	
-	it {should validate_presence_of :name }
+	it { should validate_presence_of :name }
 	
 	pending "write a validator to check presence of brew name in brewtoad"
 	pending "write a function to retreive a hash of data from brewtoad on the brew"
