ca4d549fdf834fb034e3be6ce2875b0d8e07ea55
markpeace <mark@learnsomestuff.com>
2013-08-04 22:12:06 +0100

    fixed update/delete inconsistencies

--- app/models/step.rb
+++ app/models/step.rb
@@ -17,25 +17,29 @@ class Step < ActiveRecord::Base
 	def move(direction)
 		if direction==:up then
 			return if self.order==1
-			Step.where(:brew_id=>self.brew_id, :order=>self.order-1).first.update_attributes(:order=>self.order)
-			self.update_attributes(:order=>self.order-1)
+			Step.where(:brew_id=>self.brew_id, :order=>self.order-1).first.update_columns(:order=>self.order)
+			self.update_columns(:order=>self.order-1)
 		elsif direction==:down then
 			return if self.order==Step.where(:brew_id=>self.brew_id).count
-			Step.where(:brew_id=>self.brew_id, :order=>self.order+1).first.update_attributes(:order=>self.order)
-			self.update_attributes(:order=>self.order+1)
+			Step.where(:brew_id=>self.brew_id, :order=>self.order+1).first.update_columns(:order=>self.order)
+			self.update_columns(:order=>self.order+1)
 		end 
 		self.autocalculate_dates
 	end
 	
 	
-	after_create :autocalculate_dates
+	after_save :autocalculate_dates
+	after_destroy :autocalculate_dates
+	
 	def autocalculate_dates
 		d=self.brew.date
 		i=0
+		o=1
 		Step.where(:brew_id=>self.brew_id).order("[order] ASC").each do |s|
-			s.update_attributes(:date=>d + i)
+			s.update_columns(:date=>d + i, :order=>o)
 			i=s.days.to_i
 			d=s.date
+			o=o+1
 		end
 	end
 
