7d174be1b40d51be0564ca78e3cacaf4d1288348
markpeace <mark@learnsomestuff.com>
2013-08-04 10:30:06 +0100

    respecced to accomodate XML scraping

--- spec/models/brew_spec.rb
+++ spec/models/brew_spec.rb
@@ -2,12 +2,27 @@ require 'spec_helper'
 
 describe Brew do
 
+	it "should be valid when everything is in place" do
+		FactoryGirl.build(:brew).should be_valid
+	end
+
 	it { should belong_to :user }
 	it { should validate_presence_of :user }
 	
 	it { should validate_presence_of :name }
 	
 	pending "write a validator to check presence of brew name in brewtoad"
-	pending "write a function to retreive a hash of data from brewtoad on the brew"
+
+	describe "it should have a function that scrapes data from brewtoad" do
+	
+		it "should return parsed data from xml" do
+			b=FactoryGirl.create(:brew)
+			b.xml.should_not be_nil
+		end
+	
+		it "should raise an error when a non-valid name is given" do
+			FactoryGirl.build(:brew).should_not be_valid
+		end
+	end
 
 end
