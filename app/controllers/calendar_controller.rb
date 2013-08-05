bdaf786d4a482a528c1c54a9e84ae64c6f0d3818
markpeace <mark@learnsomestuff.com>
2013-08-05 15:23:35 +0100

    added user calendar

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -12,11 +12,17 @@ class CalendarController < ApplicationController
 				cal = Calendar.new
 				
 				@steps.each do |s|
+					confirmation_link=root_url + "steps/#{s.id}/complete"
 					cal.event do 
 						dtstart	s.date
 						dtend s.date
 						summary "#{s.brew.name}: #{s.title} (#{s.days} days)"
-					end
+						description "Click The Following Link To Mark This Step as Complete: " + confirmation_link
+						alarm do
+							summary "Tadpole Notification: Tomorrow, #{s.brew.name} should be transferred to #{s.title}"
+							trigger "-P8H"
+						end
+					end					
 				end
 				
 				cal.publish
