d2e89fe8e19df58fad184b807f8747e2764b66a3
markpeace <mark@learnsomestuff.com>
2013-08-05 08:04:23 +0100

    basic calendar working

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,12 +2,26 @@ class CalendarController < ApplicationController
 
 	skip_before_filter :authenticate_user!
 	
+	include Icalendar
+	
 	def index
 		@steps=Step.where(:user_id=>params[:id]).order("date DESC")
 		respond_to do |format|
 			format.html { render :index }
 			format.ics do
-				render :index
+				cal = Calendar.new
+				
+				@steps.each do |s|
+					cal.event do 
+						dtstart	s.date
+						dtend s.date
+						summary "#{s.brew.name}: #{s.title} (#{s.days} days)"
+					end
+				end
+				
+				cal.publish
+				headers['Content-Type'] = "text/calendar; charset=UTF-8"
+				render :text => cal.to_ical, :layout=>false
 			end
 		end
 	end
