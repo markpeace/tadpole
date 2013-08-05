d652582e8a2d084461812341d42d73780228f45a
markpeace <mark@learnsomestuff.com>
2013-08-05 21:38:09 +0100

    better calendar serving

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -5,29 +5,41 @@ class CalendarController < ApplicationController
 	include Icalendar
 	
 	def index
-		@steps=Step.where(:user_id=>params[:id], :completed=>false).order("date DESC")
 		respond_to do |format|
-			format.html { render :index }
+			format.html do
+				@steps=Step.where(:user_id=>params[:id], :completed=>false).order("date DESC")
+				render :index
+			end
 			format.ics do
+				r=root_url
 				cal = Calendar.new
-				
-				@steps.each do |s|
-					confirmation_link=root_url + "steps/#{s.id}/complete"
-					cal.event do 
-						dtstart	s.date
-						dtend s.date
-						summary "#{s.brew.name}: #{s.title} (#{s.days} days)"
-						description "Click The Following Link To Mark This Step as Complete: " + confirmation_link
-						alarm do
-							summary "Tadpole Notification: Tomorrow, #{s.brew.name} should be transferred to #{s.title}"
-							trigger "-P8H"
+			
+				current_user.brews.each do |b|
+					steps=b.steps.where(:completed=>false).order("date ASC")
+					if steps.count>0 then
+						cal.event do
+							dtstart	b.date
+							dtend b.date
+							summary "Brew #{b.name}"
 						end
-					end					
+		
+						steps.each_with_index do |s,i|
+							cal.event do
+								dtstart	s.date
+								dtend s.date
+								summary "#{b.name} to #{s.title} for #{s.days} days"
+								description "Click this link to mark step as complete: #{r}steps/#{s.id}/complete" if i==0
+								description "You cannot complete this step as prior ones are still incomplete" if i>0
+							end
+						end
+						
+					end
 				end
 				
 				cal.publish
 				headers['Content-Type'] = "text/calendar; charset=UTF-8"
 				render :text => cal.to_ical, :layout=>false
+				
 			end
 		end
 	end
