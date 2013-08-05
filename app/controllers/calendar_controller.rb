e71e364237646a1736a4a9a8f65f9542af95f372
markpeace <mark@learnsomestuff.com>
2013-08-05 07:06:37 +0100

    format handling

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -1,4 +1,15 @@
 class CalendarController < ApplicationController
-  def index
-  end
+
+	skip_before_filter :authenticate_user!
+	
+	def index
+		@steps=Step.where(:user_id=>params[:id])
+		respond_to do |format|
+			format.html { render :index }
+			format.ics do
+				render :index
+			end
+		end
+	end
+	
 end
