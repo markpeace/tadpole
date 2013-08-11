0c218f61613efd682f3d6de69bb63e21df342cc7
markpeace <mark@learnsomestuff.com>
2013-08-11 13:13:50 +0100

    advanced specs now implemented for inventory

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -4,4 +4,20 @@ class Inventory < ActiveRecord::Base
 	
 	validates_presence_of :label
 	validates_presence_of :grams
+	
+	after_save :combine_if_duplicate
+	def combine_if_duplicate
+		
+		if e=Inventory.where("label=? AND id!=? AND user_id!=?", self.label, self.id, self.user_id).first then
+			e.update_columns(:grams=>e.grams+self.grams)
+			self.destroy 
+		end
+		
+	end
+	
+	after_save :remove_if_zero_grams
+	def remove_if_zero_grams
+		self.destroy if self.grams<1 or self.grams.nil?
+	end
+	
 end
