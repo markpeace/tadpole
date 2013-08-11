112994f40122461ada1ae5816568bfe63087169b
markpeace <mark@learnsomestuff.com>
2013-08-11 08:18:29 +0100

    added more advanced specs to inventory

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,6 +1,10 @@
 require 'spec_helper'
 
 describe Inventory do
+	
+	before :each do
+		User.destroy_all
+	end
 
 	it "should create when valid" do
 		FactoryGirl.build(:inventory).should be_valid
@@ -12,5 +16,17 @@ describe Inventory do
 	it { should validate_presence_of :label }
 	it { should validate_presence_of :grams }
 
+	it "remove an entry when its grams are set below 1" do
+		FactoryGirl.create(:inventory)
+		Inventory.first.update_attributes(:grams=>-10)
+		Inventory.count.to_i.should eq(0)
+	end
+	
+	it "if an entry is created which already exists, it should add grams instead" do
+		FactoryGirl.create(:inventory, label: 'maris otter', grams: 10)
+		FactoryGirl.create(:inventory, label: 'maris otter', grams: 10)
+		Inventory.count.should eq(1)
+		Inventory.first.grams.should eq(20)
+	end
 
 end
